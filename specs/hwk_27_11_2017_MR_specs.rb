9a81e7b3ee7b97cabe7cae046a9fa9eb9c28b259
Max Robson <maxwell.robson@gmail.com>
2017-11-28 09:31:39 +0000

    debugged specs file

--- specs/hwk_27_11_2017_MR_specs.rb
+++ specs/hwk_27_11_2017_MR_specs.rb
@@ -5,19 +5,16 @@ class TestCCStudent < MiniTest::Test
 
   def setup()
 
-    profile = CCStudent.new('The Dude', 20)
+    @profile = CCStudent.new('The Dude', 20)
 
   end
 
   def test_profile_name
-    assert_equal('The Dude', profile.name)
+    assert_equal('The Dude', @profile.name)
   end
 
   def test_profile_cohort
-    assert_equal(20, profile.cohort)
+    assert_equal(20, @profile.cohort)
   end
-
-  def test_favorite_language
-
 
 end
