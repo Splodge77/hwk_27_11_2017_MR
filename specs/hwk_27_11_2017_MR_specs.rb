865f92ba8ce2b55eff55656932745cddc8ed9e4e
Max Robson <maxwell.robson@gmail.com>
2017-11-28 09:29:03 +0000

    tests reformatted, still some holes i think

--- specs/hwk_27_11_2017_MR_specs.rb
+++ specs/hwk_27_11_2017_MR_specs.rb
@@ -3,14 +3,21 @@ require_relative '../hwk_27_11_2017_MR.rb'
 
 class TestCCStudent < MiniTest::Test
 
-  def test_profile_name
+  def setup()
+
     profile = CCStudent.new('The Dude', 20)
+
+  end
+
+  def test_profile_name
     assert_equal('The Dude', profile.name)
   end
 
   def test_profile_cohort
-    profile = CCStudent.new('The Dude', 20)
     assert_equal(20, profile.cohort)
   end
+
+  def test_favorite_language
+
 
 end
