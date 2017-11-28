37e92b768e170b3faf04519371e38cd860479a8e
Max Robson <maxwell.robson@gmail.com>
2017-11-28 08:52:41 +0000

    initial setup of class and spec

---
+++ hwk_27_11_2017_MR.rb
@@ -0,0 +1,8 @@
+class CCStusent
+
+  attr_accessor :name, :cohort
+
+  def initialize(input_name, input_cohort)
+    @name = name
+    @cohort = cohort
+    
---
+++ specs/hwk_27_11_2017_MR_specs.rb
@@ -0,0 +1,14 @@
+require 'minitest/autorun'
+require_relative '../hwk_27_11_2017_MR.rb'
+
+class TestCCStudent 'MiniTest::Test'
+
+  def test_profile_name
+    profile = CCStudent.new('The Dude', 20)
+    assert_equal('The Dude', profile.name)
+  end
+
+  def test_profile_cohort
+    profile = CCStudent.new('The Dude', 20)
+    assert_equal(20, profile.cohort)
+  end
